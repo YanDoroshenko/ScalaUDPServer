0b3cccca3e168376112d4f02236fdc229a0c1a83
YanDoroshenko <yan1994doroshenko@gmail.com>
2017-01-08 10:21:22 +0200

    Handshake works (except for packet loss guard), recieving data works with no confirmation

--- src/scala/Robot.scala
+++ src/scala/Robot.scala
@@ -1,6 +1,6 @@
 package scala
 
-import java.io.FileInputStream
+import java.io.{FileInputStream, FileOutputStream}
 import java.net.{DatagramPacket, DatagramSocket, InetAddress}
 
 /**
@@ -9,29 +9,72 @@ import java.net.{DatagramPacket, DatagramSocket, InetAddress}
 object Robot {
 
   val socket = new DatagramSocket(4545)
-  var id: (Byte, Byte, Byte, Byte) = _
+  var id: Array[Byte] = _
   val address = InetAddress.getByAddress(Array(192, 168, 56, 101).map(_.toByte))
   var length: Int = 10
 
   def handshake = {
     println("Handshake")
-    val id = Array[Byte](0, 0, 0, 0)
-    val seq = Array[Byte](0, 0)
-    val confirmation = Array[Byte](0, 0)
-    val flag: Byte = 2
-    val data = Array[Byte](1)
+    var id = Array[Byte](0, 0, 0, 0)
+    var seq = Array[Byte](0, 0)
+    var confirmation = Array[Byte](0, 0)
+    var flag: Byte = 4
+    var data = Array[Byte](1)
     val packetData = id ++ seq ++ confirmation ++ Array(flag) ++ data
     val outPacket = new DatagramPacket(packetData, packetData.length, address, 4000)
     socket.send(outPacket)
-    println("Packet sent")
     val inPacket = new DatagramPacket(packetData, packetData.length)
-    println("Incoming packet created")
     socket.receive(inPacket)
-    println("Packet recieved")
-    val response = inPacket.getData
-    println("Response got")
-    Robot.id = (response(0), response(1), response(2), response(3))
-    println(Robot.id)
+    println("Received handshake")
+    val response = inPacket.getData.map(_ & 0xff).map(_.toByte)
+    id = response.take(4)
+    seq = Array(response(4), response(5))
+    confirmation = Array(response(6), response(7))
+    flag = response(8)
+    data = response.drop(9)
+    println(id.mkString("id: [", ",", "]"))
+    println(seq.mkString("seq: [", ",", "]"))
+    println(confirmation.mkString("conf: [", ",", "]"))
+    println("flag: " + flag)
+    println(data.mkString("data" + data.length + ": [", ",", "]"))
+    Robot.id = id
+    checkHandshake(id, seq, confirmation, flag, data)
+  }
+
+  def checkHandshake(id: Array[Byte], seq: Array[Byte], confirmation: Array[Byte], flag: Byte, data: Array[Byte]) = {
+    println("Checking handshake")
+    if (id.sum == 0) {
+      System.err.println("Id is zero")
+      terminate
+    }
+    else if (seq.sum != 0) {
+      System.err.println("Seq is non-zero")
+      terminate
+    }
+    else if (confirmation.sum != 0) {
+      System.err.println("Confirmation is non-zero")
+      terminate
+    }
+    else if (flag != 4) {
+      System.err.println("Wrong flag")
+      terminate
+    }
+    else if (data.size != 1 || data(0) > 2) {
+      System.err.println("Wrong data")
+      terminate
+    }
+  }
+
+  def terminate = {
+    println("Closing connection")
+    val id = Robot.id
+    val seq = Array[Byte](0, 0)
+    val confirmation = Array[Byte](0, 0)
+    val flag: Byte = 0
+    val packetData = id ++ seq ++ confirmation ++ Array(flag)
+    val outPacket = new DatagramPacket(packetData, packetData.length, address, 4000)
+    socket.send(outPacket)
+    socket.close()
   }
 
   def send = {
@@ -47,12 +90,50 @@ object Robot {
   }
 
   def receive = {
-
+    val fos = new FileOutputStream("image.png")
+    while (!socket.isClosed) {
+      var id = Array[Byte](0, 0, 0, 0)
+      var seq = Array[Byte](0, 0)
+      var confirmation = Array[Byte](0, 0)
+      var flag: Byte = 0
+      var data: Array[Byte] = Array.fill(255)(0)
+      val packetData = id ++ seq ++ confirmation ++ Array(flag) ++ data
+      val inPacket = new DatagramPacket(packetData, packetData.length)
+      socket.receive(inPacket)
+      val response = inPacket.getData.map(_ & 0xff).map(_.toByte)
+      id = response.take(4)
+      seq = Array(response(4), response(5))
+      confirmation = Array(response(6), response(7))
+      flag = response(8)
+      data = response.drop(9)
+      println(id.mkString("id: [", ",", "]"))
+      println(seq.mkString("seq: [", ",", "]"))
+      println(confirmation.mkString("conf: [", ",", "]"))
+      println("flag: " + flag)
+      println(data.mkString("data (" + data.length + "): [", ",", "]"))
+      if (!data.forall(_ == 0)) {
+        fos.write(data)
+        id = Robot.id
+        seq = Array(0, 255).map(_.toByte)
+        confirmation = Array(1, 255).map(_.toByte)
+        flag = 0
+        data = Array(0).map(_.toByte)
+        val confirm = id ++ seq ++ confirmation ++ Array(flag) ++ data
+        val outPacket = new DatagramPacket(confirm, confirm.length, address, 4000)
+      }
+      if (flag == 1)
+        terminate
+      if (flag == 2)
+        socket.close()
+    }
+    fos.close()
   }
 
   def main(args: Array[String]): Unit = {
     /*new TestServer().start()
     send*/
     handshake
+    receive
+    terminate
   }
 }
